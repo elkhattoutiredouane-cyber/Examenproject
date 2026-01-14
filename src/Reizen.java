410344687e28b1b214bbf16d7ac514dbdb7e987b
elkhattoutiredouane-cyber <elkhattoutiredouane@gmail.com>
2026-01-14 14:05:26 +0100

    Deze code maakt een reis aan.
    Travel heeft een vertrekstation, aankomststation, vertrektijd, een trein en een lijst met personeel dat meereist.

---
+++ src/Reizen.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Reizen {
+}
+class Travel {
+    String departureStation;
+    String arrivalStation;
+    LocalDateTime departureTime;
+    Trein train;
+    List<Staff> staff;
+}
